d32127f85be05a384af0af5f5468dc540c08de8d
Frank Solleveld <fsolleveld@outlook.com>
2020-03-04 09:51:46 +0100

    made fillables immutable

--- helpers/charToBlocks.js
+++ helpers/charToBlocks.js
@@ -1,6 +1,6 @@
 const charToBlocks = (separatedArray) => {
 
-    let fillables = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const fillables = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     // Spread Operator: actually checks inside the array with the numbers.
     separatedArray.push(... fillables.slice(0, (10 - (separatedArray.length % 10))))
